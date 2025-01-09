5cfa01ff8816cda074ddf46fa77a2090041db734
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-01-09 13:45:48 +0000

    5. Create and Use a Button Component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ size, shape, onClick, children }) => {
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  const shapeClasses = {
+    "rounded-sm": "rounded-sm",
+    "rounded-md": "rounded-md",
+    "rounded-full": "rounded-full",
+  };
+
+  const buttonClass = `${sizeClasses[size]} ${shapeClasses[shape]} bg-blue-500 text-white hover:bg-blue-600 transition`;
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
